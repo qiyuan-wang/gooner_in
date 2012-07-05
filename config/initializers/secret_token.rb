a26124307ce61eadfc3dc542632f3d2834d99355
zephyrisalive <zephyrisalive@gmail.com>
2012-07-05 21:17:31 +0800

    project initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoonerIn::Application.config.secret_token = '5b0318d2466c69c9e1e8a7f9bf5b12607a11610066bbbe70fd6d410e80714aa67c853f491e4aaf7ec88ef8f9b0e2433b895a1c95bca12dc76fb53480a7cbdafc'

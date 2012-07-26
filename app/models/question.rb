593eadb6d4797f54b27f09863253858aeeff25e6
zephyrisalive <zephyrisalive@gmail.com>
2012-07-26 13:04:26 +0800

    finish people_controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,6 @@ class Question
   include Mongoid::Timestamps
   
   field :title, type: String
+  has_many :answers
+  has_and_belongs_to_many :related_players, class_name: "Player", inverse_of: :related_questions
 end

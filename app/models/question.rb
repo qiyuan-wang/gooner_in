0eaba72164c3b7bf20820ef22a28f57a3a0d9676
zephyrisalive <zephyrisalive@gmail.com>
2012-08-02 21:09:57 +0800

    poor view for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -12,10 +12,18 @@ class Question
   has_many :answers, :dependent => :delete
   has_and_belongs_to_many :related_players, class_name: "Player", inverse_of: :related_questions
   
-  def related_players=(players)
-    self.related_players.clear
+  def add_related_players(players)
     players.each do |p|
       self.related_players << Player.find(p) unless p.empty?
     end
+  end
+  
+  def update_related_players(players)
+    self.related_players.clear
+    self.add_related_players(players)
+  end
+  
+  def created_time
+    return self.created_at.to_date
   end
 end

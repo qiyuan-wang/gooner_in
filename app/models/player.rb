a01e73d352212d5bad6d1c35392165ec6a7d01a3
zephyrisalive <zephyrisalive@gmail.com>
2012-10-30 17:05:58 +0800

    add player's join and leave year

--- app/models/player.rb
+++ app/models/player.rb
@@ -10,9 +10,23 @@ class Player
   field :day_of_birth, type: Date
   field :position, type: String
   field :_id, type: String, default: ->{ e_name }
+  field :join, type: Date
+  field :leave, type: Date
 
   mount_uploader :avatar, PlayerAvatarUploader
   
   has_and_belongs_to_many :related_questions, class_name: "Question", inverse_of: :related_players
+  
+  def join_year
+    self.join.year
+  end
+  
+  def leave_year
+    if self.leave.year == Time.now.year
+      "至今"
+    else
+      self.leave.year
+    end
+  end
   
 end

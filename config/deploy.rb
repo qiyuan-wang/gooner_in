b6bf90d02e2922ecb5ed5890eb5edc8a73ddfd72
zephyrisalive <zephyrisalive@gmail.com>
2012-10-31 22:26:17 +0800

    add task for uploads of capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,10 +22,15 @@ role :db,  domain, :primary => true # This is where Rails migrations will run
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
+namespace :deploy do
 #   task :start do ; end
 #   task :stop do ; end
 #   task :restart, :roles => :app, :except => { :no_release => true } do
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
-# end
+  task :symlink_uploads do
+    run "ln -nfs #{shared_path}/uploads  #{release_path}/public/uploads"
+  end
+end
+
+after 'deploy:update_code', 'deploy:symlink_uploads'

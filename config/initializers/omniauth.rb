a8d25926955549f622e2b0bb60934febf408a148
zephyrisalive <zephyrisalive@gmail.com>
2012-12-03 09:40:25 +0800

    wait for the qq auth validation, my fault - -

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ OmniAuth.config.on_failure = Proc.new { |env|
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :weibo, "2424871994", "4e0c9f08ea04938276ca3c32913c1d72"
-  provider :qq
+  #provider :qq
 end

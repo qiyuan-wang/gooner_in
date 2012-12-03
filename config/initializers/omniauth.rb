8acd5805b87a9c43c50f16a8c086069582499741
zephyrisalive <zephyrisalive@gmail.com>
2012-12-03 12:25:50 +0800

    add douban app key and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ OmniAuth.config.on_failure = Proc.new { |env|
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :weibo, "2424871994", "4e0c9f08ea04938276ca3c32913c1d72"
-  #provider :qq
+  provider :douban, "08fecc7d9ccbca8f1afc6578d6635fad", "b8c822bfdca1d12a"
 end

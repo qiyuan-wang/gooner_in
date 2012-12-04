1c2d12f57be9b92ef3f5e8f86808bffc43261dc0
zephyrisalive <zephyrisalive@gmail.com>
2012-12-04 17:50:29 +0800

    try solve qq bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,5 +6,7 @@ OmniAuth.config.on_failure = Proc.new { |env|
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :weibo, "2424871994", "4e0c9f08ea04938276ca3c32913c1d72"
   provider :douban, "0a7ba095f168850a1f72e1c1d4398f1e", "d1a26d262cd03f2b"
-  provider :qq_connect, "100340909", "3842dd1768a7fc0ab168facb1fa09ee3"
+  provider :qq_connect, "100340909", "3842dd1768a7fc0ab168facb1fa09ee3",
+    scope: "get_info"
+  
 end

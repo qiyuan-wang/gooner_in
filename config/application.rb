dd22c457f2725593e6f9843f2e2573ef3f662241
zephyrisalive <zephyrisalive@gmail.com>
2012-12-27 11:47:13 +0800

    change the images names

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,7 @@ module GoonerIn
     # Enable the asset pipeline
     config.assets.enabled = true
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
     
   end
 end

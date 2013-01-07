72f729de629d8ff2edcbbf69303a1888b81181f2
zephyrisalive <zephyrisalive@gmail.com>
2013-01-07 16:16:32 +0800

    just for fun with ie

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,10 @@
 class StaticPagesController < ApplicationController
   layout "index", only: [:index]
   def index
+    if request.env['HTTP_USER_AGENT'] =~ /msie/i
+      redirect_to :log_in
     if logged_in?
       redirect_to answers_path
     end
-    @question = Question.first
-    @players = Player.all.limit(6)
   end
-
 end
